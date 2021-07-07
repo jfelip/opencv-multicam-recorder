1a545e3850d624f9e1b4a92f47102461523156eb
Javier Felip Leon <javier.felip.leon@gmail.com>
2021-07-07 16:06:37 -0700

    Simple camera detection class. Initial implementation.

--- opencv_camera_recorder.py
+++ opencv_camera_recorder.py
@@ -420,12 +420,35 @@ class MultiStreamVideoWriter:
             self.video_writers[i].write(frames[i])
 
 
+class V4L2CameraDetector:
+    def __init__(self, ncameras=50):
+        for id in range(ncameras):
+            cap = cv2.VideoCapture(id, cv2.CAP_V4L2)
+            if cap.isOpened():
+                print(f"Valid capture device found with id: {id}")
+                res, img = cap.read()
+                if res:
+                    print(f" - Capturing images: {cap.get(cv2.CAP_PROP_FRAME_WIDTH)}x"
+                          f"{cap.get(cv2.CAP_PROP_FRAME_HEIGHT)}@{cap.get(cv2.CAP_PROP_FPS)}")
+                else:
+                    print(f" - NOT Capturing images: {cap.get(cv2.CAP_PROP_FRAME_WIDTH)}x"
+                          f"{cap.get(cv2.CAP_PROP_FRAME_HEIGHT)}@{cap.get(cv2.CAP_PROP_FPS)}")
+
+
+            cap.release()
+
+
 if __name__ == "__main__":
-    cameras = [{"name": "c1", "id": "/dev/video0", "width": 640, "height": 360, "buffer_size": 2},
-               {"name": "c2", "id": "/dev/video1", "width": 800, "height": 448, "buffer_size": 2}]
+    # cameras = [{"name": "c1", "id": "/dev/video0", "width": 640, "height": 360, "buffer_size": 2},
+    #            {"name": "c2", "id": "/dev/video1", "width": 800, "height": 448, "buffer_size": 2}]
+
+    # cameras = [{"name": "c2", "id": "/dev/video0", "width": 800, "height": 448, "buffer_size": 2}]
+    cameras = [{"name": "c2", "id": "/dev/video0", "width": 640, "height": 480, "buffer_size": 2}]
 
     config = {"win_name": "Multi Camera Capturer :: javier.felip.leon@gmail.com",
-              "img_sep": 5, "streams": cameras, "scale_mosaic": False, "fps": 30}
+              "img_sep": 5, "streams": cameras, "scale_mosaic": False, "fps": 120}
+
+    cams = V4L2CameraDetector(20)
 
     gui = MultiStreamVideoGUI(config)
     gui.run()
