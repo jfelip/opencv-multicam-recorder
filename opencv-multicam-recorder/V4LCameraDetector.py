68e42ede23a126331b2d7c44b72b0d51f7a0eec7
Javier Felip Leon <javier.felip.leon@gmail.com>
2022-06-19 12:04:58 -0700

    Use v4l2ctl python package for camera detection

--- opencv-multicam-recorder/V4LCameraDetector.py
+++ opencv-multicam-recorder/V4LCameraDetector.py
@@ -1,22 +1,27 @@
-import cv2
-
-# TODO: Display camera name, parameters and valid modes
+import v4l2ctl
 
 
 class V4L2CameraDetector:
     def __init__(self, ncameras=50):
         for id in range(ncameras):
-            cap = cv2.VideoCapture(id, cv2.CAP_V4L2)
-            if cap.isOpened():
-                print(f"Valid capture device found with id: {id}")
-                res, img = cap.read()
-                if res:
-                    print(f" - Capturing images: {cap.get(cv2.CAP_PROP_FRAME_WIDTH)}x"
-                          f"{cap.get(cv2.CAP_PROP_FRAME_HEIGHT)}@{cap.get(cv2.CAP_PROP_FPS)}")
-                else:
-                    print(f" - NOT Capturing images: {cap.get(cv2.CAP_PROP_FRAME_WIDTH)}x"
-                          f"{cap.get(cv2.CAP_PROP_FRAME_HEIGHT)}@{cap.get(cv2.CAP_PROP_FPS)}")
-            cap.release()
+            try:
+                dev = v4l2ctl.V4l2Device(id)
+            except FileNotFoundError as e:
+                continue
+
+            # Filter out devices w/o video capturing capabilities
+            if v4l2ctl.V4l2Capabilities.VIDEO_CAPTURE not in dev.capabilities:
+                continue
+
+            print(f"Device({id}): {dev.name}")
+            print(f"Formats:")
+            for f in dev.formats:
+                print(f"|--> {f}")
+                for sz in f.sizes():
+                    intervals = list()
+                    for interval in sz.intervals():
+                        intervals.append(interval.interval.denominator)
+                    print(f"   |--> {sz.width}x{sz.height} @ {intervals}")
 
 
 if __name__ == "__main__":
